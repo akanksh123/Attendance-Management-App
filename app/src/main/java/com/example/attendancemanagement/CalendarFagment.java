7ed0ea272edb9f9234dfe8da13512c83bc1875cd
akanksh123 <akankshbekal@gmail.com>
2020-03-26 21:45:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/attendancemanagement/CalendarFagment.java
@@ -0,0 +1,80 @@
+package com.example.attendancemanagement;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CalendarFagment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar,
+                container, false);
+
+        TableLayout stk = (TableLayout)view.getRootView().findViewById(R.id.table_main);
+        TableRow tbrow0 = new TableRow(getActivity());
+        TextView tv0 = new TextView(getActivity());
+        tv0.setText(" Sl.No ");
+        tv0.setTextColor(Color.WHITE);
+        tbrow0.addView(tv0);
+        TextView tv1 = new TextView(getActivity());
+        tv1.setText(" Subject Name ");
+        tv1.setTextColor(Color.WHITE);
+        tbrow0.addView(tv1);
+        TextView tv2 = new TextView(getActivity());
+        tv2.setText(" Classes Held ");
+        tv2.setTextColor(Color.WHITE);
+        tbrow0.addView(tv2);
+        TextView tv3 = new TextView(getActivity());
+        tv3.setText(" Classes Attended ");
+        tv3.setTextColor(Color.WHITE);
+        tbrow0.addView(tv3);
+        TextView tv4 = new TextView(getActivity());
+        tv4.setText(" Percentage ");
+        tv4.setTextColor(Color.WHITE);
+        tbrow0.addView(tv4);
+        stk.addView(tbrow0);
+        for (int i = 0; i < 6; i++) {
+            TableRow tbrow = new TableRow(getActivity());
+            TextView t1v = new TextView(getActivity());
+            t1v.setText("" + i);
+            t1v.setTextColor(Color.WHITE);
+            t1v.setGravity(Gravity.CENTER);
+            tbrow.addView(t1v);
+            TextView t2v = new TextView(getActivity());
+            t2v.setText("Product " + i);
+            t2v.setTextColor(Color.WHITE);
+            t2v.setGravity(Gravity.CENTER);
+            tbrow.addView(t2v);
+            TextView t3v = new TextView(getActivity());
+            t3v.setText("Rs." + i);
+            t3v.setTextColor(Color.WHITE);
+            t3v.setGravity(Gravity.CENTER);
+            tbrow.addView(t3v);
+            TextView t4v = new TextView(getActivity());
+            t4v.setText("" + i * 15 / 32 * 10);
+            t4v.setTextColor(Color.WHITE);
+            t4v.setGravity(Gravity.CENTER);
+            tbrow.addView(t4v);
+            TextView t5v = new TextView(getActivity());
+            t5v.setText("" + i * 15 / 32 * 10);
+            t5v.setTextColor(Color.WHITE);
+            t5v.setGravity(Gravity.CENTER);
+            tbrow.addView(t5v);
+            stk.addView(tbrow);
+        }
+        return view;
+
+    }
+}
